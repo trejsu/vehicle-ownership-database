58dcd807398743744eaf3682ed37c7bf265ef446
Paulina Koziol <pk.koziol49@gmail.com>
2019-06-15 17:53:23 +0200

    reformating App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 
 import VehicleAddition from "./components/VehicleAddition";
@@ -7,7 +6,7 @@ import VehicleAddition from "./components/VehicleAddition";
 import "./App.css";
 
 class App extends Component {
-    state = {storageValue: 0, web3: null, accounts: null, contract: null};
+    state = {web3: null, accounts: null};
 
     componentDidMount = async () => {
         try {
@@ -17,17 +16,8 @@ class App extends Component {
             // Use web3 to get the user's accounts.
             const accounts = await web3.eth.getAccounts();
 
-            // Get the contract instance.
-            const networkId = await web3.eth.net.getId();
-            const deployedNetwork = SimpleStorageContract.networks[networkId];
-            const instance = new web3.eth.Contract(
-                SimpleStorageContract.abi,
-                deployedNetwork && deployedNetwork.address,
-            );
+            this.setState({web3, accounts});
 
-            // Set web3, accounts, and contract to the state, and then proceed with an
-            // example of interacting with the contract's methods.
-            this.setState({web3, accounts, contract: instance}, this.runExample);
         } catch (error) {
             // Catch any errors for any of the above operations.
             alert(
@@ -37,40 +27,12 @@ class App extends Component {
         }
     };
 
-    runExample = async () => {
-        const {accounts, contract} = this.state;
-
-        // Stores a given value, 5 by default.
-        await contract.methods.set(5).send({from: accounts[0]});
-
-        // Get the value from the contract to prove it worked.
-        const response = await contract.methods.get().call();
-
-        // Update state with the result.
-        this.setState({storageValue: response});
-    };
-
     render() {
         if (!this.state.web3) {
             return <div>Loading Web3, accounts, and contract...</div>;
         } else {
             return <VehicleAddition number={6}/>
         }
-        // return (
-        //   <div className="App">
-        //     <h1>Good to Go!</h1>
-        //     <p>Your Truffle Box is installed and ready.</p>
-        //     <h2>Smart Contract Example</h2>
-        //     <p>
-        //       If your contracts compiled and migrated successfully, below will show
-        //       a stored value of 5 (by default).
-        //     </p>
-        //     <p>
-        //       Try changing the value stored on <strong>line 40</strong> of App.js.
-        //     </p>
-        //     <div>The stored value is: {this.state.storageValue}</div>
-        //   </div>
-        // );
     }
 }
 
