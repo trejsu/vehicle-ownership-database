e02284a34b30ed1e8b0b7f332c535f36659fa203
Kamil Grybos <grybos.kamil@gmail.com>
2019-06-15 17:04:40 +0200

    Add bootstrap with comboselect of vehicletype

--- client/src/components/VehicleAddition.js
+++ client/src/components/VehicleAddition.js
@@ -1,24 +1,41 @@
 import React, {Component} from "react";
 
 class VehicleAddition extends Component {
-    state = {number: 0};
+    state = {};
+    defaultVehicleType = "car";
 
     componentDidMount = async () => {
-        this.setState({number: 5})
+        this.setState({vehicleType: this.defaultVehicleType})
+    };
+
+    handleVehicleTypeChanged = (event) => {
+        this.setState({vehicleType: event.target.value})
     };
 
     render() {
         return (
-            <div>
-                <div>
-                    Hello, with state number = {this.state.number}
+            <div className={"row"}>
+                <div style={{margin: "20px"}}>
+                        <span>
+                            Select type of vehicle
+                        </span>
+                    <div className="col-sm-3">
+                        <select
+                            className="form-control"
+                            onChange={this.handleVehicleTypeChanged}
+                            defaultValue={this.state.vehicleType}
+                        >
+                            <option value="car">Car</option>
+                            <option value="bike">Bike</option>
+                        </select>
+                    </div>
                 </div>
                 <div>
-                    Hello, with props number = {this.props.number}
+                    Selected type of vehicle = '{this.state.vehicleType}'
                 </div>
             </div>
         );
     }
 }
 
-export default  VehicleAddition;
+export default VehicleAddition;
