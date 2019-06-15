6bcfdcf9f45dcccd9b7a00bb7df7730e8180543e
Paulina Koziol <pk.koziol49@gmail.com>
2019-06-15 17:24:23 +0200

    adding vehicle to contract

--- contracts/VehicleOwnershipDatabase.sol
+++ contracts/VehicleOwnershipDatabase.sol
@@ -2,4 +2,26 @@ pragma solidity ^0.5.8;
 
 contract VehicleOwnershipDatabase {
 
+    enum VehicleType { Car, Bike }
+
+    struct Vehicle {
+        VehicleType vType;
+        string model;
+        address owner;
+        bool exists;
+    }
+
+    mapping (bytes32 => Vehicle) public waitingForApprovals;
+
+    modifier isNotInPending(bytes32 _id) {
+        require(!waitingForApprovals[_id].exists);
+        _;
+    }
+
+    function addVehicle(bytes32 _id, string memory _vehicleModel, uint _vehicleType) isNotInPending(_id) public {
+
+        Vehicle memory newVehicle = Vehicle(VehicleType(_vehicleType), _vehicleModel, msg.sender, true);
+
+        waitingForApprovals[_id] = newVehicle;
+    }
 }
--- test/vehicleOwnershipDatabase.js
+++ test/vehicleOwnershipDatabase.js
@@ -1,9 +1,22 @@
 const VehicleOwnershipDatabase = artifacts.require("./VehicleOwnershipDatabase.sol");
 
 contract("VehicleOwnershipDatabase", accounts => {
-  it("should load basic contract instance", async () => {
+  it("should add car to pending vehicles", async () => {
     const vehicleOwnershipDatabaseInstance = await VehicleOwnershipDatabase.deployed();
 
-    assert.notEqual(vehicleOwnershipDatabaseInstance, null, "Contract instance is null.");
+    const carVin = "1ZVBP8AM0C5265607";
+    const carModel = "Ford Fiesta";
+    const vehicleType = 0;
+
+    const id = web3.utils.fromAscii(carVin);
+
+    await vehicleOwnershipDatabaseInstance.addVehicle(id, carModel, vehicleType, { from: accounts[0] });
+
+    const storedVehicle = await vehicleOwnershipDatabaseInstance.waitingForApprovals.call(id);
+
+    assert.equal(storedVehicle[0].toNumber(), 0, "Car type should be stored.");
+    assert.equal(storedVehicle[1], carModel, "Car model should be stored.");
+    assert.equal(storedVehicle[2], accounts[0], "Car owner should be stored.");
+    assert.equal(storedVehicle[3], true, "Car should exist.");
   });
 });
