8342fb2a784b3180ea10ec1d2fd44a86ed8a6cf2
trejsu <trejsu@gmail.com>
2019-06-15 16:16:25 +0200

    Fix truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -5,10 +5,10 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    test: {
+    develop: {
       host: "127.0.0.1",
       port: 7545,
-      network_id: "*"
+      network_id: "5777"
     }
   }
 };
