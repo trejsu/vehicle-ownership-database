8d247f2f75b376a18167777fcf0cfa65258e1468
Paulina Koziol <pk.koziol49@gmail.com>
2019-06-15 22:53:13 +0200

    adding vehicle approvals in contract

--- contracts/VehicleOwnershipDatabase.sol
+++ contracts/VehicleOwnershipDatabase.sol
@@ -11,23 +11,80 @@ contract VehicleOwnershipDatabase {
         bool exists;
     }
 
-    mapping (bytes32 => Vehicle) public waitingForApprovals;
-    bytes32[] public pendingIds;
+    struct VehicleTemp {
+        VehicleType vType;
+        string model;
+        address owner;
+        bool exists;
+        mapping(address => bool) approvals;
+        uint approvalsNumber;
+    }
 
-    modifier isNotInPending(bytes32 _id) {
+    mapping (bytes32 => VehicleTemp) public waitingForApprovals;
+    mapping (bytes32 => Vehicle) public vehicleRegistry;
+    bytes32[] public pendingIds;
+    bytes32[] public existingIds;
+
+    modifier canBeRegistered(bytes32 _id) {
         require(!waitingForApprovals[_id].exists);
+        require(!vehicleRegistry[_id].exists);
         _;
     }
 
-    function addVehicle(bytes32 _id, string memory _vehicleModel, uint _vehicleType) isNotInPending(_id) public {
-        Vehicle memory newVehicle = Vehicle(VehicleType(_vehicleType), _vehicleModel, msg.sender, true);
+    modifier canApprove(bytes32 _id, address _sender) {
+        require(waitingForApprovals[_id].exists);
+        require(!waitingForApprovals[_id].approvals[_sender]);
+        _;
+    }
+
+
+    function addVehicle(bytes32 _id, string memory _vehicleModel, uint _vehicleType) canBeRegistered(_id) public {
+        VehicleTemp memory newVehicle = VehicleTemp(VehicleType(_vehicleType), _vehicleModel, msg.sender, true, 0);
         pendingIds.push(_id);
         waitingForApprovals[_id] = newVehicle;
     }
 
     function getPendingIds() public view returns (bytes32[] memory _ids) {
-
         return pendingIds;
     }
 
+    function approveVehicle(bytes32 _id) canApprove(_id, msg.sender) public {
+        VehicleTemp storage vehicle = waitingForApprovals[_id];
+
+        //check if approving user is not owner
+        require(vehicle.owner != msg.sender);
+
+        vehicle.approvals[msg.sender] = true;
+        vehicle.approvalsNumber += 1;
+
+        //check if vehicle has already two approvals
+        if(vehicle.approvalsNumber == 2 ) {
+
+            // creating existing in registry vehicle
+            Vehicle memory createdVehicle = Vehicle({
+                vType: vehicle.vType,
+                model: vehicle.model,
+                owner: vehicle.owner,
+                exists : true
+                });
+
+            vehicleRegistry[_id] = createdVehicle;
+            existingIds.push(_id);
+
+            //removing vehicle from waiting for approval list
+            delete waitingForApprovals[_id];
+
+            //removing vehicle from pendingIds list
+            for(uint i=0; i < pendingIds.length; i++) {
+                if(pendingIds[i] == _id) {
+                    delete pendingIds[i];
+                }
+            }
+        }
+
+    }
+
+    function getRegisteredIds() public view returns (bytes32[] memory ids) {
+        return existingIds;
+    }
 }
--- test/vehicleOwnershipDatabase.js
+++ test/vehicleOwnershipDatabase.js
@@ -9,6 +9,7 @@ contract("VehicleOwnershipDatabase", accounts => {
     const vehicleType = 0;
 
     const id = web3.utils.fromAscii(carVin);
+    console.log(id);
 
     await vehicleOwnershipDatabaseInstance.addVehicle(id, carModel, vehicleType, { from: accounts[0] });
 
@@ -23,4 +24,39 @@ contract("VehicleOwnershipDatabase", accounts => {
     assert.equal(storedVehicle[3], true, "Car should exist.");
     assert.equal(returnedId, carVin, "Vin should be added to ids list.");
   });
+
+  it("should move vehicle to registered after two approvals", async () => {
+    const vehicleOwnershipDatabaseInstance = await VehicleOwnershipDatabase.deployed();
+
+    const carVin = "1ZVBP8AM0AAAA5607";
+    const carModel = "Ford Escort";
+    const vehicleType = 0;
+
+    const id = web3.utils.fromAscii(carVin);
+    console.log(id);
+
+    // adding car first to pending ids
+    await vehicleOwnershipDatabaseInstance.addVehicle(id, carModel, vehicleType, { from: accounts[0] });
+
+    // approving by two different users
+    await vehicleOwnershipDatabaseInstance.approveVehicle(id, { from: accounts[1] });
+    await vehicleOwnershipDatabaseInstance.approveVehicle(id, { from: accounts[2] });
+
+    const storedVehicle = await vehicleOwnershipDatabaseInstance.vehicleRegistry.call(id);
+    const existingIds = await vehicleOwnershipDatabaseInstance.getRegisteredIds.call();
+    const returnedId = web3.utils.toAscii(existingIds[0]).replace(/\u0000/g, '');
+
+    const notStoredVehicle = await vehicleOwnershipDatabaseInstance.waitingForApprovals.call(id);
+
+    //check if stored in vehicle registry
+    assert.equal(storedVehicle[0].toNumber(), 0, "Car type should be stored.");
+    assert.equal(storedVehicle[1], carModel, "Car model should be stored.");
+    assert.equal(storedVehicle[2], accounts[0], "Car owner should be stored.");
+    assert.equal(storedVehicle[3], true, "Car should exist.");
+    assert.equal(returnedId, carVin, "Vin should be added to ids list.");
+
+    //check if removed from pending
+    assert.equal(notStoredVehicle[3], false, "Car should not exist in pending.");
+  });
 });
+
