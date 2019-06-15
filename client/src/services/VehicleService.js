27e86a39473bfcba982af2350e6cd054a17cdc33
trejsu <trejsu@gmail.com>
2019-06-15 18:00:38 +0200

    Add VehicleService initialization with web3

--- client/src/services/VehicleService.js
+++ client/src/services/VehicleService.js
@@ -1,6 +1,26 @@
+import SimpleStorageContract from "../contracts/SimpleStorage";
+
 export default class VehicleService {
-  constructor() {
-    console.log('Vehicle service created.');
+  constructor(contract) {
+    if (typeof contract === 'undefined') {
+      throw new Error('Cannot instantiate VehicleService directly. Use init function instead.');
+    }
+    this.contract = contract;
+  }
+
+  static init(web3) {
+    return VehicleService.initContract(web3)
+    .then(contract => new VehicleService(contract));
+  }
+
+  static initContract(web3) {
+    return web3.eth.net.getId().then(networkId => {
+      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      return new web3.eth.Contract(
+        SimpleStorageContract.abi,
+        deployedNetwork && deployedNetwork.address,
+      );
+    });
   }
 
   addCar(car) {
--- client/src/test/VehicleService.test.js
+++ client/src/test/VehicleService.test.js
@@ -1,7 +1,16 @@
 import VehicleService from "../services/VehicleService";
+import Web3 from "web3";
 
-it('has addCar and addBike methods', () => {
-  const service = new VehicleService();
+// todo: get host from config
+const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
+const web3 = new Web3(provider);
+
+it('has addCar and addBike methods', async () => {
+  const service = await VehicleService.init(web3);
   service.addCar({});
   service.addBike({});
+});
+
+it('should fail when instantiated through constructor', () => {
+  expect(() => new VehicleService()).toThrow(Error);
 });
