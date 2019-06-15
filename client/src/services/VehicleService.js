1fb03ec76ad50d40f58f6348e562bb6547fceaa6
trejsu <trejsu@gmail.com>
2019-06-15 16:29:56 +0200

    Add VehicleService interface

---
+++ client/src/services/VehicleService.js
@@ -0,0 +1,13 @@
+export default class VehicleService {
+  constructor() {
+    console.log('Vehicle service created.');
+  }
+
+  addCar(car) {
+    console.log('Adding car.');
+  }
+
+  addBike(bike) {
+    console.log('Adding bike.');
+  }
+}
