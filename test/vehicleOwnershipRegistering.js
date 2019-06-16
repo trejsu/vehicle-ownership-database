ac190022db25359f9553d1ac82fc466d565c01ad
Paulina Koziol <pk.koziol49@gmail.com>
2019-06-16 19:31:51 +0200

    contract tests refactoring

--- test/vehicleOwnershipRegistering.js
+++ test/vehicleOwnershipRegistering.js
@@ -79,64 +79,6 @@ contract("VehicleOwnershipDatabase", accounts => {
 
     });
 
-    it("should add vehicle to transfer list", async () => {
-        const carVin = "2ZVBP8AM0AAAA5607";
-        const carModel = "Ford Mondeo";
-        const vehicleType = 0;
-
-        const id = web3.utils.fromAscii(carVin);
-
-        // adding car first to pending ids
-        await contract.addVehicle(id, carModel, vehicleType, {from: accounts[0]});
-
-        // approving by two different users
-        await contract.approveVehicle(id, {from: accounts[1]});
-        await contract.approveVehicle(id, {from: accounts[2]});
-
-        const newOwner = accounts[1];
-
-        await contract.transferVehicle(id, newOwner, {from: accounts[0]});
-
-        const transferredVehicle = await contract.waitingForTransfers.call(id);
-
-        assert.equal(transferredVehicle[0], accounts[0], "Car's owner should be stored.");
-        assert.equal(transferredVehicle[1], newOwner, "Car's next owner should be stored.");
-        assert.equal(transferredVehicle[2], true, "Car should exists in waiting for transfer structure.");
-
-        const transferredIds = await contract.getTransferIds.call();
-        const transferredId = web3.utils.toAscii(transferredIds[0]).replace(/\u0000/g, '');
-
-        assert.equal(transferredId, carVin, "Car should exists in waiting for transfer structure.");
-
-    });
-
-    it("should make transfer between users", async () => {
-        const carVin = "2ZVBPNOC0AAAA5607";
-        const carModel = "Ford Mondeo";
-        const vehicleType = 0;
-
-        const id = web3.utils.fromAscii(carVin);
-
-        // adding car first to pending ids
-        await contract.addVehicle(id, carModel, vehicleType, {from: accounts[0]});
-
-        // approving by two different users
-        await contract.approveVehicle(id, {from: accounts[1]});
-        await contract.approveVehicle(id, {from: accounts[2]});
-
-        const newOwner = accounts[1];
-
-        await contract.transferVehicle(id, newOwner, {from: accounts[0]});
-        await contract.approveTransfer(id, {from: newOwner});
-
-        const storedVehicle = await contract.vehicleRegistry.call(id);
-        const transferredVehicle = await contract.waitingForTransfers.call(id);
-
-        assert.equal(storedVehicle[2], newOwner, "Car should have new owner.");
-        assert.equal(transferredVehicle[2], false, "Transfer should not exists in waiting for transfers.");
-
-    });
-
     it("2 approvals should be required", async () => {
         // given
         const car = {
