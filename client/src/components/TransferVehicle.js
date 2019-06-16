7b663e5a4d8c73be44edb2dc9b6941ebf9c153da
Kamil Grybos <grybos.kamil@gmail.com>
2019-06-16 21:11:11 +0200

    Transfer cleanUp

--- client/src/components/TransferVehicle.js
+++ client/src/components/TransferVehicle.js
@@ -1,28 +1,58 @@
 import React, {Component} from "react";
 
 export default class TransferVehicle extends Component {
-    state = {error: false, transferred: false};
+    state = {
+        error: false,
+        transferred: false,
+        address: "",
+        id: "",
+        blocked: false
+    };
     vehicleService = this.props.vehicleService;
 
     handleIdChanged = (event) => {
-        this.setState({id: event.target.value})
+        this.setState({
+            id: event.target.value
+        })
     };
 
     handleAddressChanged = (event) => {
-        this.setState({address: event.target.value})
+        this.setState({
+            address: event.target.value
+        })
     };
 
-    handleRequestClicked = () => {
+    handleRequestClicked() {
         const id = this.state.id;
         const address = this.state.address;
-        this.setState({id: null, address: null, error: false, transferred: false});
+
+        this.setState({
+            id: "",
+            address: ""
+        });
+
+        // this.setState({
+        //     blocked: true
+        // });
+
         this.vehicleService.transferVehicle(id, address)
             .then(() => {
-                this.setState({transferred: true})
+                this.setState({
+                    transferred: true
+                })
             })
             .catch(() => {
-                this.setState({error: true});
-            });
+                this.setState({
+                    error: true
+                });
+            })
+            .finally(() => {
+                this.setState({
+                    id: "",
+                    address: "",
+                    blocked: false
+                });
+            })
     };
 
     getTransferPositiveInfo = () => {
@@ -52,35 +82,37 @@ export default class TransferVehicle extends Component {
     render() {
         return (
             <div>
-                <div>
-                    <div className={"transfer-vehicle-title"}>
-                        Transfer Vehicle
+                <div className={"transfer-vehicle-title"}>
+                    Transfer Vehicle
+                </div>
+
+                <div className={"transfer-vehicle-panel"}>
+                    <div>
+                        <input
+                            placeholder={"Vehicle ID"}
+                            value={this.state.id}
+                            disabled={this.state.blocked}
+                            onChange={this.handleIdChanged}/>
                     </div>
-                    <div className={"transfer-vehicle-panel"}>
-                        <div>
-                            <input
-                                placeholder={"Receiver address"}
-                                defaultValue={this.state.address}
-                                onChange={this.handleAddressChanged}/>
-                        </div>
 
-                        <div>
-                            <input
-                                placeholder={"Vehicle ID"}
-                                defaultValue={this.state.id}
-                                onChange={this.handleIdChanged}/>
-                        </div>
-
-                        <div>
-                            <button type={"button"}
-                                    className={"btn btn-primary"}
-                                    onClick={this.handleRequestClicked}>
-                                Request
-                            </button>
-                        </div>
-
-                        {this.getTransferResult()}
+                    <div>
+                        <input
+                            placeholder={"Receiver address"}
+                            value={this.state.address}
+                            disabled={this.state.blocked}
+                            onChange={this.handleAddressChanged}/>
                     </div>
+
+                    <div>
+                        <button type={"button"}
+                                className={"btn btn-primary"}
+                                disabled={this.state.blocked}
+                                onClick={this.handleRequestClicked.bind(this)}>
+                            Request
+                        </button>
+                    </div>
+
+                    {this.getTransferResult()}
                 </div>
             </div>
         )
